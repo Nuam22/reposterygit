a0d1b4cd6d44c0483604c703cfcc0eb343189296
Nuamin <markonal15@gmail.com>
2022-04-03 15:47:45 -0400

    first java github test

---
+++ J_projectOne/src/com/dayonepractice/TestClass.java
@@ -0,0 +1,14 @@
+package com.dayonepractice;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello everyone");
+String EmpName = "Sam Smith";
+double empSalary = 30000.00;
+double empBonus=600;
+double TotalSalary = empSalary + empBonus;// 3000 + 600=
+	}
+
+}
